f8339e633136417237360415bd7514dd34851c0a
matheustonetto <tonetto.m@gmail.com>
2020-09-04 17:25:29 -0300

    Criacao de rota user-wallet

--- src/main/java/com/wallet/entity/User.java
+++ src/main/java/com/wallet/entity/User.java
@@ -20,6 +20,7 @@ public class User implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1693850165739564098L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
